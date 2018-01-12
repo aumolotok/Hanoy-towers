4907f92bd5eddf51f8f612a9bd1dff444d3d57b4
Unknown <aumolotok@mail.ru>
2018-01-12 19:12:47 +0300

    First commit

---
+++ HanoyTowers/HanoyTowers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HanoyTowers {
+	class Program {
+		static void Main(string[] args) {
+			Tower tower1 = new Tower(5);
+			Tower tower2 = new Tower();
+
+			tower1.Give(tower2);
+			tower1.Give(tower2);
+		}
+	}
+}
---
+++ HanoyTowers/HanoyTowers/Tower.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HanoyTowers {
+	class Tower {
+
+		public Tower(int maxDick)
+		{
+			for (int i = maxDick; i > 0; i--)
+			{
+				Discs.Push(i);
+			}
+		}
+
+		public Tower() { }
+
+
+		public bool Push(int income)
+		{
+			if (income < TopValue)
+			{
+				Discs.Push(income);
+				return true;
+			}
+			return false;
+		}
+
+		public bool Give(Tower acceptor)
+		{
+			if (acceptor.Push(Discs.Peek()))
+			{
+				Discs.Pop();
+				return true;
+			}
+			return false;
+		}
+
+		private int TopValue => Discs.Peek();
+
+		
+
+		private Stack<int> Discs = new Stack<int>();
+	}
+}
